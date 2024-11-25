b120a698aa697150f63af9ccf69bbfd3960cb09e
MonwaEbrahim <154889050+MonwaEbrahim@users.noreply.github.com>
2024-11-26 00:40:38 +0300

    small ;

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$A2URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
+$A2URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
 try{
     $response = file_get_contents($A2URL);
